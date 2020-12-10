3b8224cfa2d58f2998a101a17b46b67205a2d35c
valentinyak <63512456+valentinyak@users.noreply.github.com>
2020-12-10 20:11:41 +0200

    Add files from HW-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import shortid from 'shortid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: shortid.generate(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: shortid.generate(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: shortid.generate(), name: 'Eden Clements', number: '645-17-79' },
+      { id: shortid.generate(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = e => {
+    const { nameInput, phoneInput } = e.target.form;
+    const { checkAddedContact, clearInputs } = this;
+
+    e.preventDefault();
+
+    if (checkAddedContact(e)) {
+      alert(`${nameInput.value} is already in contacts`);
+      return;
+    }
+
+    this.setState(prevState => {
+      const contacts = prevState.contacts.concat({
+        id: shortid.generate(),
+        name: nameInput.value,
+        number: phoneInput.value,
+      });
+
+      clearInputs(e);
+
+      return {
+        contacts,
+        filter: '',
+      };
+    });
+  };
+
+  clearInputs(e) {
+    const { nameInput, phoneInput } = e.target.form;
+
+    nameInput.value = '';
+    phoneInput.value = '';
+  }
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  checkAddedContact = e => {
+    return this.state.contacts.find(
+      contact => contact.name === e.target.form.nameInput.value,
+    );
+  };
+
+  deleteContact = e => {
+    this.setState({
+      contacts: this.state.contacts.filter(
+        contact => contact.id !== e.target.id,
+      ),
+    });
+  };
+
+  render() {
+    const {
+      getFilteredContacts,
+      addContact,
+      changeFilter,
+      deleteContact,
+    } = this;
+    const filteredContacts = getFilteredContacts();
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={addContact} />
+
+        <h2>Contacts</h2>
+        <Filter onChange={changeFilter} />
+
+        <ContactList contacts={filteredContacts} onClick={deleteContact} />
+      </div>
+    );
+  }
 }
 
 export default App;
